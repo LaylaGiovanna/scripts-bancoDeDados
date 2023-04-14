f31d1636f886ee2d77076c5558414eda27ef0d70
LAYLA GIOVANNA FURTADO DA SILVA <22282096@CFP127ALUNO.LOCAL>
2023-04-14 11:30:02 -0300

    Aprendendo scripts de banco e integrando

---
+++ model/DAO/aluno.DAO.js
@@ -0,0 +1,32 @@
+/*********************************************** 
+ * Objetivo: Realizar a interação do Aluno com o Banco de Dados
+ * Data: 14/04/2023
+ * Autora: Layla Giovanna
+ * Versão: 1.0
+************************************************/
+
+
+//Inserir um novo registro do Banco de Dados
+const insertAluno = function(dadosAluno){
+
+}
+//Atualizar um registro do Banco de Dados
+const updateAluno = function(dadosAluno){
+
+}
+//Excluir um registro do Banco de Dados
+const deleteAluno = function(id){
+
+}
+
+//Retorna todos os registros do Banco de Dados
+const selectAllAluno = function(){
+
+}
+
+//Retorna um registro filtrado pelo ID do Banco de Dados
+const selectByIdAluno = function(id){
+
+}
+
+
